44b6f928dbbdcc843af61a60869c9344f39e4157
Emre DURGUNLU <emredurgunlu@gmail.com>
2025-10-13 15:59:51 +0300

    .

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import tailwindcss from "@tailwindcss/vite"
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	vite: {
+		plugins: [tailwindcss()],
 		server: {
 			watch: {
 				usePolling: true,
